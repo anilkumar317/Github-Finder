d5342e8128639a564b3d9e7578334efbab0efd90
ANIL KUMAR <nangianilkumar216@gmail.com>
2025-02-15 11:30:36 +0530

    Initaial commit

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Alert from './components/layouts/Alert';
+import Home from './pages/Home';
+import About from './pages/About';
+import NotFound from './pages/NotFound';
+import { GithubProvider } from './context/github/Githubcontext';
+import { AlertProvider } from './context/alert/AlertContext';
+import User from './pages/User';
+
+function App() {
+  return (
+    <GithubProvider>
+      <AlertProvider>
+        <Router>
+          <div className="flex-col justify-between ">
+            <Navbar title="Github Finder" />
+            <Alert />
+            <main className="container  mx-auto px-3 pb-12 ">
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/user/:login" element={<User />} />
+                <Route path="/notfound" element={<NotFound />} />
+                <Route path="/*" element={<NotFound />} />
+              </Routes>
+            </main>
+          </div>
+        </Router>
+      </AlertProvider>
+    </GithubProvider>
+  );
+}
+
+export default App;
---
+++ src/context/alert/AlertReducer.js
@@ -0,0 +1,12 @@
+import AlertContext from './AlertContext';
+
+export const alertReducer = (state, action) => {
+  switch (action.type) {
+    case 'SET_ALERT':
+      return action.payload;
+    case 'REMOVE_ALERT':
+      return null;
+    default:
+      return state;
+  }
+};
