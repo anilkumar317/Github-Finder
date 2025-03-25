317aae74f817595c1cb4251481592c99cbce8c7b
ANIL KUMAR <nangianilkumar216@gmail.com>
2025-03-25 22:27:17 +0530

    Another Change

--- src/components/users/UserSearch.jsx
+++ src/components/users/UserSearch.jsx
@@ -1,5 +1,5 @@
 import { useState, useContext } from 'react';
-import GithubContext from '../../context/github/Githubcontext';
+import GithubContext from '../../context/github/GithubContext';
 import AlertContext from '../../context/alert/AlertContext';
 import { searchUsers } from '../../context/github/GithubActions';
 
