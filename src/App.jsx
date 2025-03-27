add48c0d392de7e0370b6cb10298096447e727c7
ANIL KUMAR <nangianilkumar216@gmail.com>
2025-03-27 12:54:51 +0530

    SomeChanges

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Alert from './components/layouts/Alert';
+import Home from './components/pages/Home';
+import About from './components/pages/About';
+import NotFound from './components/pages/NotFound';
+import { GithubProvider } from './context/github/GithubContext.jsx';
+import { AlertProvider } from './context/alert/AlertContext';
+import User from './components/pages/User';
+
+function App() {
+  return (
+    <GithubProvider>
+      <AlertProvider>
+        <Router>
+          <div className="flex-col justify-betweenv  ">
+            <Navbar title="Github Finder" />
+            <Alert />
+            <main className="container  mx-auto px-3 pb-12 ">
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/user/:login" element={<User />} />
+                <Route path="/notfound" element={<NotFound />} />
+                <Route path="/*" element={<NotFound />} />
+              </Routes>
+            </main>
+          </div>
+        </Router>
+      </AlertProvider>
+    </GithubProvider>
+  );
+}
+
+export default App;
